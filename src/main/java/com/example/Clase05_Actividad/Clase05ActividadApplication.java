fba20506b55c6626ec0df26f48dd600e18da66cd
aZ3Dx <71582669+aZ3Dx@users.noreply.github.com>
2022-04-25 16:33:17 -0500

    Add files via upload

---
+++ src/main/java/com/example/Clase05_Actividad/Clase05ActividadApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clase05_Actividad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase05ActividadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase05ActividadApplication.class, args);
+	}
+
+}
