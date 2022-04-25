aac748664d651ee113f3eaa016220ac0a7da5d75
aZ3Dx <71582669+aZ3Dx@users.noreply.github.com>
2022-04-25 16:34:21 -0500

    Add files via upload

---
+++ src/test/java/com/example/Clase05_Actividad/Clase05ActividadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clase05_Actividad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase05ActividadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
